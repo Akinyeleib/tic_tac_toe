f517f514f73622f534842fe066ad8d8758d21801
Akinyele Ibrahim <iakinyele3@gmail.com>
2022-11-29 23:07:15 +0100

    Board lined

---
+++ lib/helper.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CreateBoxes extends StatelessWidget {
+  var control = TextEditingController();
+
+  CreateBoxes(this.control);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 120,
+      height: 120,
+      child: Card(
+        
+        color: Colors.purple,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'X',
+              style: TextStyle(fontSize: 40),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CreateRow extends StatelessWidget {
+  
+  List<TextEditingController> controls;
+
+  CreateRow(this.controls);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CreateBoxes(controls[0]),
+        CreateBoxes(controls[1]),
+        CreateBoxes(controls[2]),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,40 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'helper.dart';
 
 void main() => runApp(TicTacToe());
 
 class TicTacToe extends StatelessWidget {
+  var control1 = TextEditingController();
+  var control2 = TextEditingController();
+  var control3 = TextEditingController();
+
+  late List<TextEditingController> controls;
+
+  // void addControls() {
+  //   controls.add(control1);
+  //   controls.add(control2);
+  //   controls.add(control3);
+  // }
+
   @override
   Widget build(BuildContext context) {
+    // addControls();
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('TicTacToe'),
-          centerTitle: true,
-        ),
-        body: Text("We're here..."),
-      ),
-    );
+        theme: ThemeData(primarySwatch: Colors.purple),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('TicTacToe'),
+            centerTitle: true,
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CreateRow([control1, control2, control3]),
+              CreateRow([control1, control2, control3]),
+              CreateRow([control1, control2, control3]),
+            ],
+          ),
+        ));
   }
 }
